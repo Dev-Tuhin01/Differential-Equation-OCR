e882cccbcaa7545aa740dcded78ad8a7067a0493
dev-tuhin01 <dev.tuhin.1@gmail.com>
2023-05-05 21:14:50 +0530

    Added functionalities to get images Added frontend to get images from user Added backend to store the image

---
+++ client/src/uploader.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import Dropzone, { DropzoneState } from "react-dropzone";
+
+const Uploader = () => {
+  const [uploadedFile, setUploadedFile] = useState<string | null>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    axios
+      .post<{ filename: string }>("http://localhost:8000/upload", formData)
+      .then((res) => setUploadedFile(res.data.filename))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const renderDropzoneContent = (props: DropzoneState) => {
+    const { getRootProps, getInputProps } = props;
+    return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop a file here, or click to select a file</p>
+        <input type="file" onChange={(e) => onDrop(e.target.files!)} />
+        {uploadedFile && (
+          <img
+            src={`http://localhost:8000/images/${uploadedFile}`}
+            alt="Uploaded file"
+          />
+        )}
+      </div>
+    );
+  };
+
+  return <Dropzone onDrop={onDrop}>{renderDropzoneContent}</Dropzone>;
+};
+
+export default Uploader;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,34 @@
-import express, { Request, Response } from "express";
+import express from "express"; // const express = require("express");
+import multer from "multer"; // const multer = require("multer");
+import path from "path"; //const path = require("path");
 
 const app = express();
+const port = 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello World");
+// Set up multer to handle file uploads
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
+
+// Set up routes
+app.get("/", (req, res) => {
+  res.send("working");
 });
 
-app.listen(5000);
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.send({ filename: req.file.filename });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
